161e1eea6b618a344765e750c4c129e3a805b310
burioden <hashizoe.y@gmail.com>
2023-02-03 19:36:10 +0900

    dart教材用ディレクトリ追加

--- sample/lib/01_text.dart
+++ sample/lib/01_text.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 // ----------------------------------------
-// 文字を表示しよう //zoe branch反映テスト
+// 文字を表示しよう
 // ----------------------------------------
 // ignore: camel_case_types
 class NextPage_text extends StatelessWidget {
