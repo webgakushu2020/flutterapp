364b92ad395910946a255a57e5bd57e533b5fb15
burioden <hashizoe.y@gmail.com>
2023-02-18 02:25:59 +0900

    分割ファイル追加

---
+++ sample/widget01/03_row.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Profile',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+          appBar: AppBar(title: const Text('自己紹介')),
+          body: Center(
+            child: Container(
+              child: Column(
+                children: [
+                  // ★：①childrenの中にRow()を入れる
+                  Row(
+                    // ★：②childrenの中にTextを入れる（①②を全てに対し行う）
+                    children: [Text('名前'), Text('ぞえ')],
+                  ),
+                  Row(
+                    children: [Text('住み'), Text('東京')],
+                  ),
+                  Row(
+                    children: [Text('誕生日'), Text('02/20')],
+                  ),
+                  Row(
+                    children: [Text('趣味'), Text('音楽')],
+                  ),
+                  Row(
+                    children: [Text('ひとこと'), Text('アプリ作ろうぜ！')],
+                  )
+                ],
+              ),
+            ),
+          )),
+    );
+  }
+}
---
+++ sample/widget01/05_border.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Profile',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+          backgroundColor: Colors.yellow.shade50,
+          appBar: AppBar(title: const Text('自己紹介')),
+          body: Center(
+            child: Container(
+              width: 300,
+              height: 500,
+              // ★：①decorationを設定する
+              decoration: BoxDecoration(
+                  // ★：②背景色の設定
+                  color: Colors.white,
+                  // ★：③ボーダーをつける
+                  border: Border.all(color: Colors.brown, width: 2),
+                  // ★：④ボーダーに丸みをつける
+                  borderRadius: BorderRadius.circular(10)),
+              child: Column(
+                children: [
+                  Row(
+                    children: [Text('名前'), Text('ぞえ')],
+                  ),
+                  Row(
+                    children: [Text('住み'), Text('東京')],
+                  ),
+                  Row(
+                    children: [Text('誕生日'), Text('02/20')],
+                  ),
+                  Row(
+                    children: [Text('趣味'), Text('音楽')],
+                  ),
+                  Row(
+                    children: [Text('ひとこと'), Text('アプリ作ろうぜ！')],
+                  )
+                ],
+              ),
+            ),
+          )),
+    );
+  }
+}
